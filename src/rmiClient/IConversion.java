a220abdb2872dfd0cc6d76877f3e3415624a61ca
AmriWissal <wissalamri841@gmail.com>
2023-11-09 23:43:07 +0100

    INIT

---
+++ src/rmiClient/IConversion.java
@@ -0,0 +1,9 @@
+package rmiClient;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface IConversion extends Remote {
+	double convertirMontant(double mt) throws RemoteException;
+
+}
