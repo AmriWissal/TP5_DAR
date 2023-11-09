d121ecb8a9b7d5aae26f6f387a03a77ed9bf9e18
AmriWissal <147332238+AmriWissal@users.noreply.github.com>
2023-11-10 00:04:34 +0100

    Delete src/rmiClient directory

--- src/rmiClient/IConversion.java
+++
@@ -1,9 +0,0 @@
-package rmiClient;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface IConversion extends Remote {
-	double convertirMontant(double mt) throws RemoteException;
-
-}
